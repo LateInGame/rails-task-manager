735fe0bf7a15ef76c72d48b24580e0027f0abaf0
David Deus <davidafonsodeus@outlook.com>
2022-11-15 12:08:49 +0000

    link_to all tasks (back link)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
