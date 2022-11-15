79554dc9c02936ec34e89e7724d72d760261eab4
David Deus <davidafonsodeus@outlook.com>
2022-11-15 13:32:21 +0000

    before action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -8,7 +9,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def create
@@ -18,23 +19,26 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params) # Will raise ActiveModel::ForbiddenAttributesError
     redirect_to tasks_path(@tasks)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     # No need for app/views/tasks/destroy.html.erb
     redirect_to tasks_path(@tasks), status: :see_other
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
